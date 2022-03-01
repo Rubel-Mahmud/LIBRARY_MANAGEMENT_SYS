7a7ae99dfb5a30889b3f2dae90b201b936002dc2
Rubel-Mahmud <rmsoft.com@gmail.com>
2022-03-01 20:56:06 +0600

    Publisher, Book model created

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Publisher, Book
 
-# Register your models here.
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'department', 'semister')
+
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from students.models import Department
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'publishers'
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    semisters = (
+        ('1','1st'),
+        ('2','2nd'),
+        ('3','3rd'),
+        ('4','4th'),
+        ('5','5th'),
+        ('6','6th'),
+        ('7','7th'),
+        ('8','8th'),
+    )
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=50)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    semister = models.CharField(choices=semisters, max_length=50)
+    amount = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'books'
+
+    def __str__(self):
+        return f'{self.name} - {self.publisher}'
+
+    def book_return(self):
+        """
+        When any student will return book then book amount will increase one more
+        Because a student can take only one book at a time.
+        """
+        self.amount += 1
+
+    def book_delivery(self):
+        """
+        When any student will take book then book amount will decrease one more
+        Because a student can take only one book at a time.
+        """
+        self.amount -= 1
+
+    def is_book_available(self):
+        """
+        If totall amount of any book is zero then this method will return 0.
+        Otherwise it will return 1.
+        """
+        if self.amount != 0:
+            return 1
+        else:
+            return 0
